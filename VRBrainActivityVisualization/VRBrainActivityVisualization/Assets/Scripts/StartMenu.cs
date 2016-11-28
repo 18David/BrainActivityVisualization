b4009f0238dab8601d87b13268a36cf1087be435
18David <18goncalves.david@gmail.com>
2016-11-28 11:09:17 +0100

    VR SUPPORT HTC VIVE WITH UNITY START DEV

---
+++ VRBrainActivityVisualization/VRBrainActivityVisualization/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("addnamefile");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
